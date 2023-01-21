b5bddca85cd39e15c2b7135fda20c890ca133732
Canho <caioaugusto1337@hotmail.com>
2023-01-20 23:37:19 -0300

    first commit

---
+++ calculadoraa/calculadoraa/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace calculadoraa
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Pressione \"Enter\" para encerrar ");
+            Console.ReadLine();
+        }
+    }
+}
